59b71dd3f4f4b5bddb5c09b2595353330870a3dc
Jerico <jtsanchez9@up.edu.ph>
2026-03-26 22:00:40 +0800

    refactor: removed all frontend related files

--- healph-frontend/src/pages/landingpage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import LandingPageComponents from "../components/landingpagecomponents";
-
-const LandingPage = () => {
-  return <LandingPageComponents />;
-};
-
-export default LandingPage;
