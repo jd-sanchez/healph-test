728b4a86d37a257fe81363c1929ce919df89552f
Jerico <jtsanchez9@up.edu.ph>
2026-03-26 21:58:55 +0800

    refactor: addeds fields needed for flutter app

--- healph-backend/models/daily_intake.js
+++ healph-backend/models/daily_intake.js
@@ -5,12 +5,18 @@ const Schema = mongoose.Schema;
 
 const daily_intakeSchema = new Schema({
   uid: { type: Schema.ObjectId, ref: "User", required: true },
-  date: { type: String, required: true, minLength: 9 ,maxLength: 11 },
+  date: { type: String, required: true, minLength: 9, maxLength: 11 },
   sleephrs: { type: Number, min: 0, required: true },
   waterglass: { type: Number, min: 0, required: true },
   dailycal: { type: Number, min: 0, required: true },
   steps: { type: Number, min: 0, required: true },
   hale: { type: Number, min: 0, required: true },
+  mealDiversity: { type: Number, min: 0 },
+  proteins: { type: Number, min: 0 },
+  carbs: { type: Number, min: 0 },
+  fats: { type: Number, min: 0 },
+  fibers: { type: Number, min: 0 },
+  sugars: { type: Number, min: 0 },
 });
 
 daily_intakeSchema.plugin(mongoosePaginate);
--- healph-backend/models/meal.js
+++ healph-backend/models/meal.js
@@ -7,17 +7,19 @@ const mealSchema = new Schema({
   uid: { type: Schema.ObjectId, ref: "User", required: true },
   dailyid: { type: Schema.ObjectId, ref: "Intake", required: true },
   datetime: { type: Date, required: true },
-  pic: {type: String},
+  pic: { type: String },
   cal: { type: Number, min: 0 },
   fat: { type: Number, min: 0 },
   carbs: { type: Number, min: 0 },
   proteins: { type: Number, min: 0 },
-  sodium: {type: Number, min: 0},
-  waste: {type: Number, min: 0},
+  fibers: { type: Number, min: 0 },
+  sugars: { type: Number, min: 0 },
+  sodium: { type: Number, min: 0 },
+  waste: { type: Number, min: 0 },
   mealdesc: { type: String },
-  mealname: {type: String },
-  foodgroups: { type : [String] }
-})
+  mealname: { type: String },
+  foodgroups: { type: [String] },
+});
 
 mealSchema.plugin(mongoosePaginate);
 
