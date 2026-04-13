7ac3a217055427265fffc50a5672bc17868a71bc
Jerico <jtsanchez9@up.edu.ph>
2026-04-14 02:40:15 +0800

    feat: add isCompleted field to event

--- healph-backend/models/event.js
+++ healph-backend/models/event.js
@@ -8,6 +8,7 @@ const eventSchema = new mongoose.Schema({
     location: { type: String, required: true },
     imageUrl: { type: String, required: true },
     participants: { type: [String], default: [] },
+    isCompleted: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
 });
 
